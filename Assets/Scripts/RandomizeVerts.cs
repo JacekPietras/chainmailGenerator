99fa86c2186235a66c99be51a4b5cecb7e9e3779
Jacek Pietras <jock69pl@gmail.com>
2019-01-27 19:55:00 +0100

    Seed improvemnt

--- Assets/Scripts/RandomizeVerts.cs
+++ Assets/Scripts/RandomizeVerts.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomizeVerts : MonoBehaviour {
-    [Range(0.01f, 0.1f)]
+    [Range(0.01f, 0.9f)]
     public float skewFactor = 0.05f;
     [Range(0f, 2.0f)]
     public float speedFactor = 1f;
@@ -12,12 +12,13 @@ public class RandomizeVerts : MonoBehaviour {
     private Vector3[] orginalVertices;
     private Vector3[] sinFactors;
 
-
     void Start() {
         Mesh mesh = GetComponent<MeshFilter>().mesh;
         orginalVertices = (Vector3[])mesh.vertices.Clone();
         sinFactors = new Vector3[orginalVertices.Length];
-
+        if (seed > 0) {
+            Random.seed = (int)seed * 10000;
+        }
         int i = 0;
         while (i < orginalVertices.Length) {
             sinFactors[i].x = Random.Range(-1, 1);
